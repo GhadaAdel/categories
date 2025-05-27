77a792fdb7f93a1a29c2a40e84bb0dd411f9e214
GhadaAdel <gh.adel.aa@gmail.com>
2025-05-27 16:41:04 +0300

    orders - order items model and migration

---
+++ app/Models/Order.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'status',
+        'total_price',
+        'shipping_address',
+        'billing_address',
+        'placed_at',
+        'shipped_at',
+        'payment_method'
+    ];
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
