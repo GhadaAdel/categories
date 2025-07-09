d99d120332f31c668e01689753a0e43d01e947dc
GhadaAdel <gh.adel.aa@gmail.com>
2025-07-09 13:11:50 +0300

    Top 5 sold products chart

---
+++ app/Filament/Widgets/TopProductsChart.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\OrderItem;
+use Filament\Widgets\ChartWidget;
+use Illuminate\Support\Facades\DB;
+
+class TopProductsChart extends ChartWidget
+{
+    protected static ?string $heading = 'Top Products Chart';
+    protected int|string|array $columnSpan = 'full';
+    protected static ?string $maxHeight = '400px';
+
+    protected function getData(): array
+    {
+        $topProducts = OrderItem::select('product_id', DB::raw('SUM(quantity) as total'))
+            ->groupBy('product_id')
+            ->orderByDesc('total')
+            ->with('product')
+            ->take(5)
+            ->get();
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Units Sold',
+                    'data' => $topProducts->pluck('total')->toArray(),
+                ],
+            ],
+            'labels' => $topProducts->pluck('product.name')->toArray(),
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'bar';
+    }
+}
